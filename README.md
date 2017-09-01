29de7e31b4ba208a9ad52cf26818c4fbbfdadda5
theonestep4 <theonestep4@gmail.com>
2017-09-01 13:46:39 -0400

    Updating Tutorial, add license and README

---
+++ README.md
@@ -0,0 +1,12 @@
+# [Intro to PHP + Elastic](https://github.com/theonestep4/intro-php-elastic)
+ 
+ This is a beginner tutorial on using PHP and Elastic.
+
+## Requirements
+- [Docker](docker.com)
+
+## Commands
+| What do you want to do?  | Commands you should run in a shell |
+|---|---|
+|Start development environment| Run `docker-compose up -d` |
+|Run PHP commands to elastic db |1. `docker-compose exec php-fpm bash` <br> 2. `php -f index.php` |
--- index.php
+++ index.php
@@ -14,40 +14,57 @@
 
 # Indexing a document
 
-    $params = [
-        'index' => 'developers',
-        'type' => 'developer',
-        'body' => [
-             'name' => 'John',
-              'last_name' => 'Snow'
-        ],
-        'id'  => 'AV407MObsplhUyYBgzF9'
-    ];
+    // $params = [
+    //     'index' => 'characters',
+    //     'type' => 'character',
+    //     'body' => [
+    //          'first_name' => 'Jon',
+    //           'last_name' => 'Snow'
+    //     ],
+    //     'id' => '001'        
+    // ];
 
-    $response = $client->index($params);
+    // $response = $client->index($params);
 
 # Getting a document
 
-    $params = [
-        'index' => 'developers',
-        'type' => 'developer',
-        'id' => 'AV407MObsplhUyYBgzF9'
-    ];
-    $response = $client->get($params);
+    // $params = [
+    //     'index' => 'characters',
+    //     'type' => 'character',
+    //     'id' => '001'
+    // ];
+    // $response = $client->get($params);
+
+# Updating a document
+
+    // $params = [
+    //     'index' => 'characters',
+    //     'type' => 'character',
+    //     'body' => [
+    //                 'doc' => [
+    //                     'house' => 'House Stark',
+    //                     'place' => 'Tower of Joy',
+    //                     'first_name' =>'Aegon',
+    //                     'last_name' => 'Targarye'
+    //                 ]
+    //             ], 
+    //     'id' => '001'
+    // ];
+    // $response = $client->update($params);
 
 # Searching
 
     // $params = [
-    //     'index' => 'developers',
-    //     'type' => 'developer',
+    //     'index' => 'characters',
+    //     'type' => 'character',
     //     'body' => [
     //         'query' => [
     //             'match' => [
-    //                 'name' => 'John'
+    //                 'name' => 'Aegon'
     //             ]
     //         ]
     //     ]
     // ];
     // $response = $client->search($params);
-    var_dump($response);
+
     
